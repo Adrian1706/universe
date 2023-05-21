deff035e00fef32f4d6082eb6410184618dde57e
Adrian1706 <adrianjbuenop@gmail.com>
2023-05-21 09:29:30 -0500

    feat: :rocket: Parte 2 del proyecto
    Hasta ahorita hice el segundo HTML para la busqueda de las diferentes categorias, hice js para ver como puedo empezar a recorrer el API

---
+++ js/universe.js
@@ -0,0 +1,19 @@
+async function luna (){
+	const url = 'https://celestial-objects.p.rapidapi.com/galaxies';
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '4d3abdd95amsh42b11a056be5dccp19703cjsn96a51d54275a',
+			'X-RapidAPI-Host': 'celestial-objects.p.rapidapi.com'
+		}
+	};
+
+	try {
+		const response = await fetch(url, options);
+		const result = await response.json();
+		console.log(result);
+		
+	} catch (error) {
+		console.error(error);
+	}
+}
